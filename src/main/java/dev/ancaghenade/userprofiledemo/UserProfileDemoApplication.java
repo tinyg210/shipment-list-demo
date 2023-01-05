56d31830e8791d42d97aedba9de0cbb9e2d9a04e
AncaGhenade <anca.ghenade@ilogics.com>
2023-01-05 19:57:01 +0100

    project init

---
+++ src/main/java/dev/ancaghenade/userprofiledemo/UserProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.ancaghenade.userprofiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserProfileDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/ancaghenade/userprofiledemo/UserProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ancaghenade.userprofiledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
