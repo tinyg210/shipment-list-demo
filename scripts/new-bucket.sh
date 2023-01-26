514398ef3ec902ae0f4018a7ac8c57529830d8f2
AncaGhenade <anca.ghenade@ilogics.com>
2023-01-26 02:26:31 +0100

    - add readme - small fixes

--- scripts/new-bucket.sh
+++ scripts/new-bucket.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # For demo purpose only;
-# Access key and secret key are set and using `aws configure`
+# Access key and secret key are set by using `aws configure`
 # to manage the aws cli on your local machine
 
 # Set the name of the S3 bucket
 bucket_name="shipment-list-demo-bucket"
 aws_region="eu-central-1"
 
-# Create the S3 bucket
-aws s3 mb s3://$bucket_name
-
-# Set the region for the S3 bucket
-aws s3api create-bucket --bucket $bucket_name --region $aws_region
+# Create the S3 bucket and set the region
+aws s3api create-bucket --bucket $bucket_name --region eu-central-1 \
+--create-bucket-configuration LocationConstraint=eu-central-1
 
 # Set the bucket policy
 aws s3api put-bucket-acl --bucket $bucket_name --acl private
