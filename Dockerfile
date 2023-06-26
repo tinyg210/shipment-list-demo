d600d00058a9f866993b0f0076e623c4a35d613c
AncaGhenade <ancaghenade@gmail.com>
2023-06-26 12:00:59 +0200

    add dockerfile for backend and frontend and replace hardcoded host with env var arg

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Stage 1: Build the application
+FROM maven:3.8.2-openjdk-17 AS build
+
+WORKDIR /usr/src/app
+
+COPY pom.xml ./
+RUN mvn dependency:go-offline
+
+COPY src ./src/
+RUN mvn package -DskipTests
+
+# Stage 2: Run the application
+FROM openjdk:17-alpine
+
+WORKDIR /usr/app
+
+COPY --from=build /usr/src/app/target/*.jar app.jar
+
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/shipment-list-frontend/src/App.js
+++ src/main/shipment-list-frontend/src/App.js
@@ -11,7 +11,7 @@ const Shipments = () => {
   const [refreshKey, setRefreshKey] = useState(0); // hack0: Add refreshKey state to force refresh
 
   const fetchShipments = () => {
-    axios.get("http://localhost:8081/api/shipment").then(res => {
+    axios.get("https://${process.env.BACKEND_API_HOST}/api/shipment").then(res => {
       console.log(res);
       setShipments(res.data)
     }).then(() => {
@@ -28,7 +28,7 @@ const Shipments = () => {
   }, [isFetchingComplete]);
 
   function handleRemove(shipmentId) {
-    axios.delete(`http://localhost:8081/api/shipment/${shipmentId}`)
+    axios.delete(`https://${process.env.BACKEND_API_HOST}/api/shipment/${shipmentId}`)
     .then(res => {
       console.log(res.data)
       const newList = shipments.filter(
@@ -72,7 +72,7 @@ const Shipments = () => {
             }}>
               <div>
                 <Dropzone {...shipment}/>
-                <img src={`http://localhost:8081/api/shipment/${shipment.shipmentId}/image/download?t=${Date.now()}`} //hack1: cache busting to refresh
+                <img src={`https://${process.env.BACKEND_API_HOST}/api/shipment/${shipment.shipmentId}/image/download?t=${Date.now()}`} //hack1: cache busting to refresh
                      alt={placeholder}
                      style={{objectFit: "contain"}}/>
 
@@ -118,7 +118,7 @@ function Dropzone(
     formData.append("file", file);
 
     axios.post(
-        `http://localhost:8081/api/shipment/${shipmentId}/image/upload`,
+        `https://${process.env.BACKEND_API_HOST}/api/shipment/${shipmentId}/image/upload`,
         formData,
         {
           headers: {
