0627866cb3f6a3eeda8055b6d13993e9bfb6b939
AncaGhenade <ancaghenade@gmail.com>
2023-07-12 12:22:28 +0200

    add install tf & tflocal

--- terraform/run-tf.sh
+++ terraform/run-tf.sh
@@ -1,3 +1,11 @@
+apt-get update
+apt-get install -y wget unzip
+wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
+unzip terraform_0.14.7_linux_amd64.zip
+mv terraform /usr/local/bin/
+
+terraform --version
+
 pip install terraform-local
 
 tflocal init
